4773138cf87582d9261eb65c7185db3cbab115d7
Joseph Gutierrez <josephmauricio23@hotmail.com>
2022-04-01 17:51:09 -0500

    integracion sidebar con navbard

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public showMenu = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggle() {
+    this.showMenu = !this.showMenu;
   }
-
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { faBars, faBell } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
@@ -9,7 +9,13 @@ export class NavbarComponent implements OnInit {
   public faBars = faBars;
   public faBell = faBell;
 
+  @Output() toggleMenu = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  toggle() {
+    this.toggleMenu.emit();
+  }
 }
