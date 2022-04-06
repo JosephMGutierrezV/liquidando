6a5a9cd14049ea0668507391c54a444ae52ce566
Joseph Gutierrez <josephmauricio23@hotmail.com>
2022-04-06 09:04:58 -0500

    Finalizando pruebas pdf

--- src/app/modules/liquidation/liquide/liquide.component.ts
+++ src/app/modules/liquidation/liquide/liquide.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GeneratePdfService } from '../../../services/generate-pdf.service';
 
 @Component({
   selector: 'app-liquide',
@@ -7,62 +6,7 @@ import { GeneratePdfService } from '../../../services/generate-pdf.service';
   styleUrls: ['./liquide.component.scss'],
 })
 export class LiquideComponent implements OnInit {
-  constructor(private generatePdfService: GeneratePdfService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  downloadPdf() {
-    const data = {
-      fileName: 'liquidacion',
-      text: {
-        demandante: 'DEMANDANTE',
-        demandado: 'DEMANDADO',
-        radicado: 'RADICADO',
-        juzgado: 'JUZGADO',
-        interesesTotal: 'INTERESES TOTAL',
-        capital: 'CAPITAL',
-        totalGran: 'TOTAL GRAN TOTAL',
-        initialDate: 'FECHA INICIAL',
-        finalDate: 'FECHA FINAL',
-        titleAbonos: 'ABONOS',
-        tasaInteres: 'TASA DE INTERES',
-        date: 'FECHA',
-        abono: 'ABONO',
-        capitalizacion: 'CAPITALIZACION',
-        titleInteres: 'INTERESES',
-        tasa: 'TASA',
-        intereses: 'INTERESES',
-        total: 'TOTAL',
-      },
-      value: {
-        demandante: 'DEMANDANTE',
-        demandado: 'DEMANDADO',
-        radicado: 'RADICADO',
-        juzgado: 'JUZGADO',
-        intereses: 'INTERESES',
-        capital: 'CAPITAL',
-        total: 'TOTAL',
-        initialDate: 'FECHA INICIAL',
-        finalDate: 'FECHA FINAL',
-        tasaInteres: 'TASA DE INTERES',
-      },
-      dataGridAbono: [
-        {
-          date: '01/01/2020',
-          abono: '$100',
-          capitalizacion: '$100',
-        },
-      ],
-      dataGridIntereses: [
-        {
-          date: '01/01/2020',
-          tasa: '1.5',
-          intereses: '$100',
-          capital: '$100',
-          total: '$100',
-        },
-      ],
-    };
-    this.generatePdfService.generatePDF(data);
-  }
 }
