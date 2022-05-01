1fc9ccc9adae4278cada64a345ddd2813c923cda
Joseph Gutierrez <josephmauricio23@hotmail.com>
2022-05-01 15:04:59 -0500

    Correcion navbar nombre user

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,20 +1,40 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AppState } from './../../store/app.reducer';
+import { Store } from '@ngrx/store';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  OnDestroy,
+} from '@angular/core';
 import { faBars, faBell, faUserLock } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   public faBars = faBars;
   public faBell = faBell;
   public faUserLock = faUserLock;
+  public nameUser = '';
+  public subscriptions: any[] = [];
 
   @Output() toggleMenu = new EventEmitter();
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.store.select('auth').subscribe((auth) => {
+        this.nameUser = auth.user;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 
   toggle() {
     this.toggleMenu.emit();
