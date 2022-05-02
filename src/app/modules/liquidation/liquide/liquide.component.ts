672b4ef929c660560f23dfac31c45cbcca9a27ad
Joseph Gutierrez <josephmauricio23@hotmail.com>
2022-05-02 10:56:31 -0500

    Se agrega plantilla excel y fav icon

--- src/app/modules/liquidation/liquide/liquide.component.ts
+++ src/app/modules/liquidation/liquide/liquide.component.ts
@@ -1,18 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import { debounceTime, fromEvent, Observable, pluck } from 'rxjs';
 
 @Component({
   selector: 'app-liquide',
   templateUrl: './liquide.component.html',
   styleUrls: ['./liquide.component.scss'],
 })
-export class LiquideComponent implements OnInit {
+export class LiquideComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('inputSettled') inputSettled!: ElementRef;
+
   public showInputOtherRate = false;
 
   public isHaveNumberSettled = true;
 
+  public keyUpSettled$!: Observable<any>;
+
+  public subscriptions: any[] = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.keyUpSettled$ = fromEvent(
+      this.inputSettled.nativeElement,
+      'keyup'
+    ).pipe(debounceTime(500), pluck('target', 'value'));
+    this.subscriptions.push(
+      this.keyUpSettled$.subscribe((value: any) => {
+        if (value === '') {
+          this.isHaveNumberSettled = true;
+        } else {
+          this.isHaveNumberSettled = false;
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription: any) =>
+      subscription.unsubscribe()
+    );
+  }
 
   onItemChange(event: any) {
     if (event.target.value === 'option4') {
