95b3273ccffeebf4317a1977925ec89f2ac97651
Joseph Gutierrez <josephmauricio23@hotmail.com>
2022-05-15 08:51:47 -0500

    filtro implementado y limpieza filtros tabla historial

--- src/app/shared/p-grid/p-grid.component.ts
+++ src/app/shared/p-grid/p-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-p-grid',
@@ -8,8 +9,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PGridComponent implements OnInit {
   @Input() loading = false;
   @Input() historials: any[] = [];
+  @ViewChild('dt') dt!: Table;
+  @ViewChild('inputSearch') inputSearch!: any;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  search(event: any) {
+    this.dt.filterGlobal(event.target.value, 'contains');
+  }
+
+  clear(table: Table) {
+    this.inputSearch.nativeElement.value = '';
+    table.clear();
+  }
 }
